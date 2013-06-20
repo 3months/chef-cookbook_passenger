6df8a6deb925b5fe8c5e532d65b35dc3f6d372d4
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-20 17:10:12 +1200

    Update default root path and Passenger module output path to reflect Passenger 4.0.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,5 +9,5 @@ default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_pa
 default[:passenger][:rbenv][:enabled] = false
 default[:passenger][:rbenv][:version] = "ruby-1.9.2-p290-patched"
 default[:passenger][:rbenv][:binary] = "/usr/local/rbenv/versions/#{node[:passenger][:rbenv][:version]}/bin/ruby"
-default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/#{node[:passenger][:rbenv][:version]}/lib/ruby/gems/1.9.1/gems/passenger-#{passenger[:version]}"
-default[:passenger][:rbenv][:module_path] = "#{passenger[:rbenv][:root_path]}/ext/apache2/mod_passenger.so"
+default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/#{node[:passenger][:rbenv][:version]}/lib/ruby/gems/2.0.0/gems/passenger-#{passenger[:version]}"
+default[:passenger][:rbenv][:module_path] = "#{passenger[:rbenv][:root_path]}/libout/apache2/mod_passenger.so"
