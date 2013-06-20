6dc7738fa2b9c1dba2cbfe88e4de6540acde2227
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-20 15:09:39 +1200

    Use node passenger rbenv ruby version in configuration for setting Ruby paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,6 @@ default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_pa
 # rbenv integration
 default[:passenger][:rbenv][:enabled] = false
 default[:passenger][:rbenv][:version] = "ruby-1.9.2-p290-patched"
-default[:passenger][:rbenv][:binary] = "/usr/local/rbenv/versions/ruby-1.9.2-p290-patched/bin/ruby"
-default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/ruby-1.9.2-p290-patched/lib/ruby/gems/1.9.1/gems/passenger-#{passenger[:version]}"
+default[:passenger][:rbenv][:binary] = "/usr/local/rbenv/versions/#{node[:passenger][:rbenv][:version]}/bin/ruby"
+default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/#{node[:passenger][:rbenv][:version]}/lib/ruby/gems/1.9.1/gems/passenger-#{passenger[:version]}"
 default[:passenger][:rbenv][:module_path] = "#{passenger[:rbenv][:root_path]}/ext/apache2/mod_passenger.so"
